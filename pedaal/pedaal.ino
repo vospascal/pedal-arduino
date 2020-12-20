e8d8cd0ee85b5797a8789384def080919564a41f
Pascal Vos <vospascal@gmail.com>
2020-12-20 14:47:43 +0100

    fix clutch settings

--- pedaal/pedaal.ino
+++ pedaal/pedaal.ino
@@ -5,10 +5,10 @@
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
                    0, 0,                  // Button Count, Hat Switch Count
-                   false, false, false,     // X and Y, but no Z Axis
-                   false, false, false,   // No Rx, Ry, or Rz
-                   true, true,          // No rudder or throttle
-                   true, true, false);  // No accelerator, brake, or steering
+                   false, false, true,     // X and Y, Z Axis
+                   false, false, false,   // Rx, Ry, or Rz
+                   false, true,          // rudder or throttle
+                   false, true, false);  // accelerator, brake, or steering
 
 
 //const bool initAutoSendState = true;
@@ -19,11 +19,11 @@ int clutchValue = 0;
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  Serial.begin(115200);
   Joystick.begin();
   Joystick.setThrottle(throttleValue);
   Joystick.setBrake(brakeValue);
-  //  Joystick.setAccelerator(clutchValue);
+  Joystick.setZAxis(clutchValue);
   delay(2000);
 }
 
@@ -34,7 +34,6 @@ int outputMapClutch[6] = { 0, 15, 43, 53, 75, 100 };
 int inputMapThrottle[6] =  { 0, 20, 40, 60, 80, 100 };
 int outputMapThrottle[6] = { 0, 15, 43, 53, 75, 100 };
 
-
 int inputMapBrake[6] =  { 0, 20, 40, 60, 80, 100 };
 int outputMapBrake[6] = { 0, 15, 43, 53, 75, 100 };
 
@@ -44,18 +43,14 @@ float BrakeAfter;
 int ThrottleBefore;
 float ThrottleAfter;
 
-
-//int ClutchBefore;
-//float ClutchAfter;
+int ClutchBefore;
+float ClutchAfter;
 
 // the loop routine runs over and over again forever:
 void loop() {
 
   if (Serial.available() > 0) {
     String msg = Serial.readStringUntil('\n');
-
-
-    //    delay(50);
 
     if (msg.indexOf("Getmap") >= 0) {
       String TMAP = "TMAP:" + generateStringMap(outputMapThrottle);
@@ -78,17 +73,17 @@ void loop() {
 
     updateThrottleMap(msg);
     updateBrakeMap(msg);
-    
+    updateClutchMap(msg);
   }
 
 
-  // read the input on analog pin 0:
+  // read the input on analog pins
   int throttleRawValue = analogRead(A0);
-  int brakeRawValue = analogRead(A1);
-  //  int clutchRawValue = analogRead(A2);
+  int brakeRawValue = analogRead(A3);
+  int brakePresureRawValue = analogRead(A2);
+  int clutchRawValue = analogRead(A1);
 
   // print out the value you read:
-  //
   if (throttleRawValue <= 74) {
     ThrottleBefore = 0;
     ThrottleAfter = 0;
@@ -113,17 +108,17 @@ void loop() {
     Joystick.setBrake(BrakeAfter);
   }
 
-  //  if (clutchRawValue <= 74) {
-  //    ClutchBefore = 0;
-  //    ClutchAfter = 0;
-  //    Joystick.setAccelerator(0);
-  //  } else {
-  //    int restClutchValue = clutchRawValue - 74;
-  //
-  //    ClutchBefore = restClutchValue / 4;
-  //    ClutchAfter = multiMap<int>(ClutchBefore, inputMapClutch, outputMapClutch, 50);
-  //    Joystick.setAccelerator(ClutchAfter);
-  //  }
+  if (clutchRawValue <= 74) {
+    ClutchBefore = 0;
+    ClutchAfter = 0;
+    Joystick.setZAxis(0);
+  } else {
+    int restClutchValue = clutchRawValue - 74;
+
+    ClutchBefore = restClutchValue / 4;
+    ClutchAfter = multiMap<int>(ClutchBefore, inputMapClutch, outputMapClutch, 50);
+    Joystick.setZAxis(ClutchAfter);
+  }
 
 
   String p1 = ";";
@@ -132,9 +127,9 @@ void loop() {
 
   Serial.print("B:");
   Serial.println(BrakeBefore + p1 + BrakeAfter);
-  //
-  //  Serial.print("C:");
-  //  Serial.println(ClutchBefore + p1 + ClutchAfter);
+
+  Serial.print("C:");
+  Serial.println(ClutchBefore + p1 + ClutchAfter);
 
   Joystick.sendState(); // Update the Joystick status on the PC
   Serial.flush();
@@ -204,29 +199,48 @@ void updateThrottleMap (String msg) {
     outputMapThrottle[4] = tpart4.toInt();
     outputMapThrottle[5] = tpart5.toInt();
   }
- 
 }
 
 
-void updateBrakeMap(String msg){
-    if (msg.indexOf("BMAP:") >= 0) {
-      String striped_Bmap = msg;
-      striped_Bmap.replace("BMAP:", "");
-      String bpart0 = getValue(striped_Bmap, '-', 0);
-      String bpart1 = getValue(striped_Bmap, '-', 1);
-      String bpart2 = getValue(striped_Bmap, '-', 2);
-      String bpart3 = getValue(striped_Bmap, '-', 3);
-      String bpart4 = getValue(striped_Bmap, '-', 4);
-      String bpart5 = getValue(striped_Bmap, '-', 5);
-      outputMapBrake[0] = bpart0.toInt();
-      outputMapBrake[1] = bpart1.toInt();
-      outputMapBrake[2] = bpart2.toInt();
-      outputMapBrake[3] = bpart3.toInt();
-      outputMapBrake[4] = bpart4.toInt();
-      outputMapBrake[5] = bpart5.toInt();
-    }
-
+void updateBrakeMap(String msg) {
+  if (msg.indexOf("BMAP:") >= 0) {
+    String striped_Bmap = msg;
+    striped_Bmap.replace("BMAP:", "");
+    String bpart0 = getValue(striped_Bmap, '-', 0);
+    String bpart1 = getValue(striped_Bmap, '-', 1);
+    String bpart2 = getValue(striped_Bmap, '-', 2);
+    String bpart3 = getValue(striped_Bmap, '-', 3);
+    String bpart4 = getValue(striped_Bmap, '-', 4);
+    String bpart5 = getValue(striped_Bmap, '-', 5);
+    outputMapBrake[0] = bpart0.toInt();
+    outputMapBrake[1] = bpart1.toInt();
+    outputMapBrake[2] = bpart2.toInt();
+    outputMapBrake[3] = bpart3.toInt();
+    outputMapBrake[4] = bpart4.toInt();
+    outputMapBrake[5] = bpart5.toInt();
+  }
 }
+
+
+void updateClutchMap(String msg) {
+  if (msg.indexOf("CMAP:") >= 0) {
+    String striped_Cmap = msg;
+    striped_Cmap.replace("CMAP:", "");
+    String cpart0 = getValue(striped_Cmap, '-', 0);
+    String cpart1 = getValue(striped_Cmap, '-', 1);
+    String cpart2 = getValue(striped_Cmap, '-', 2);
+    String cpart3 = getValue(striped_Cmap, '-', 3);
+    String cpart4 = getValue(striped_Cmap, '-', 4);
+    String cpart5 = getValue(striped_Cmap, '-', 5);
+    outputMapClutch[0] = cpart0.toInt();
+    outputMapClutch[1] = cpart1.toInt();
+    outputMapClutch[2] = cpart2.toInt();
+    outputMapClutch[3] = cpart3.toInt();
+    outputMapClutch[4] = cpart4.toInt();
+    outputMapClutch[5] = cpart5.toInt();
+  }
+}
+
 
 String getValue(String data, char separator, int index)
 {
